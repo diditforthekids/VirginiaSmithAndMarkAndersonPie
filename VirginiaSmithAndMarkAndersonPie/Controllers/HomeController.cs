2af8717ca193b75bb560afda45b4bf996334fbf8
Jeff Makoroff <jmakoroff@gmail.com>
2019-03-22 19:25:56 -0500

    Add project files.

---
+++ VirginiaSmithAndMarkAndersonPie/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VirginiaSmithAndMarkAndersonPie.Models;
+using VirginiaSmithAndMarkAndersonPie.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VirginiaSmithAndMarkAndersonPie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            
+
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Virginia Smith and Mark Andersons Pie Shop",
+                Pies = pies.ToList()
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
